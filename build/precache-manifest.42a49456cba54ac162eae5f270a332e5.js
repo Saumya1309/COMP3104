831cf25c05e9c663985d49d359aabd54bf4b0285
Saumya Mistry <mistryasaumya@gmail.com>
2021-10-13 19:57:54 -0400

    adding build folder

---
+++ build/precache-manifest.42a49456cba54ac162eae5f270a332e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60000caed8bb59ce54b8",
+    "url": "/Saumya1309/COMP3104/static/css/main.82ecf3ab.chunk.css"
+  },
+  {
+    "revision": "60000caed8bb59ce54b8",
+    "url": "/Saumya1309/COMP3104/static/js/main.60000cae.chunk.js"
+  },
+  {
+    "revision": "21e04be02455a350ed5f",
+    "url": "/Saumya1309/COMP3104/static/js/runtime~main.21e04be0.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Saumya1309/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "fc391f11633339fa2f5d91dbba1359cc",
+    "url": "/Saumya1309/COMP3104/index.html"
+  }
+];
