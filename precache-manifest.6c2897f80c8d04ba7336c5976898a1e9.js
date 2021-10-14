5ddcb29d82c084d86d5cb5850d4bf5147a120c49
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-14 00:11:48 +0000

    Deploy Saumya1309/COMP3104 to github.com/Saumya1309/COMP3104.git:gh-pages

---
+++ precache-manifest.6c2897f80c8d04ba7336c5976898a1e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "521b6a3fc955c4edb54d",
+    "url": "/Saumya1309/COMP3104/static/css/main.1b9e53d2.chunk.css"
+  },
+  {
+    "revision": "521b6a3fc955c4edb54d",
+    "url": "/Saumya1309/COMP3104/static/js/main.521b6a3f.chunk.js"
+  },
+  {
+    "revision": "21e04be02455a350ed5f",
+    "url": "/Saumya1309/COMP3104/static/js/runtime~main.21e04be0.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Saumya1309/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "18627c2b1f29bcd226c964e0acffab92",
+    "url": "/Saumya1309/COMP3104/index.html"
+  }
+];
